cd058b9977484eb0528ad29ff9265038ccc98a27
azizturki99 <aziz.turky.99@gmail.com>
2023-12-23 09:23:22 +0100

    first part of sprint

---
+++ sprintmaterialpart1.js
@@ -0,0 +1,20 @@
+var carName="volvo"
+
+
+var length = 16;
+//type number 
+var lastName = "Johnson"; 
+//type string
+var isGreaterThan10 = length > 10 
+//typer boolean 
+
+
+function  square1(x) { 
+    return x * x ; 
+    } 
+
+    function largest(x,y) {
+        return Math.max(x,y)
+    }
+
+    
