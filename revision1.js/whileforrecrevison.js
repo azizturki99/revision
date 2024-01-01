f9e7757f744d9a1c3b25b863b66aa8bed37df53e
azizturki99 <aziz.turky.99@gmail.com>
2024-01-01 17:35:44 +0100

    a

--- revision1.js/whileforrecrevison.js
+++ revision1.js/whileforrecrevison.js
@@ -128,6 +128,16 @@ function sumWithWhile(n) {
     return s
 }
 -----------
+5////
+function fac(n){
+    if(n===0){
+        return 1
+    }
+    return n*fac(n-1)
+    
+}
+------------
 6////
+
 
 
